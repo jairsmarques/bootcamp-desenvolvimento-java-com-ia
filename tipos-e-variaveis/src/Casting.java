96f2229fd28d2f711327e51fda447ca04f6e2123
Jair de Sousa <jairsmarques1126@gmail.com>
2024-07-11 13:00:42 -0300

    adicionando a aula sobre tipos e variáveis

---
+++ tipos-e-variaveis/src/Casting.java
@@ -0,0 +1,13 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal;
+		
+		System.out.println(numeroCurto2);
+
+	}
+
+}
---
+++ tipos-e-variaveis/src/TiposDeDados.java
@@ -0,0 +1,21 @@
+
+public class TiposDeDados {
+
+	public static void main(String[] args) {
+		byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+		
+		System.out.println(idade);
+		System.out.println(ano);
+		System.out.println(cep);
+		System.out.println(cpf);
+		System.out.println(pi);
+		System.out.println(salario);
+
+	}
+
+}
