ca9a3bcc9008faacc5d903c19f245307fc471662
Jair de Sousa <jairsmarques1126@gmail.com>
2024-07-11 11:36:17 -0300

    Adicionando a aula sobre a anatonis das classes

---
+++ anatomia-das-classes/src/edu/jair/primeirasemana/ExemploMetodo.java
@@ -0,0 +1,19 @@
+package edu.jair.primeirasemana;
+
+public class ExemploMetodo {
+
+	public static void main(String[] args) {
+		String primeiroNome = "Jair";
+		String segundoNome = "de Sousa";
+		
+		String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+		
+		System.out.println(nomeCompleto);
+
+	}
+	
+	public static String nomeCompleto(String primeiroNome, String segundoNome) {
+		return "Resultado do método = " +  primeiroNome.concat(" ").concat(segundoNome);
+	}
+
+}
---
+++ anatomia-das-classes/src/edu/jair/primeirasemana/MinhaClasse.java
@@ -0,0 +1,20 @@
+package edu.jair.primeirasemana;
+
+public class MinhaClasse {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá turma, sejam bem-vindos!");
+		
+		String seuNome = "Jair";
+		int anoFabricacao = 2022;
+		boolean verdadeira = false;
+		anoFabricacao = 2018;
+		
+		System.out.println(seuNome);
+		System.out.println(anoFabricacao);
+		System.out.println(verdadeira);
+
+	}
+
+}
