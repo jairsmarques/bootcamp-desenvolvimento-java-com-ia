3dfb61b4983c043c8f470c1ba1c5994dd0e3b212
Jair de Sousa <jairsmarques1126@gmail.com>
2024-07-12 17:45:22 -0300

    adicionando a aula sobre operadores

---
+++ operadores/src/OperadorConcatenacao.java
@@ -0,0 +1,24 @@
+
+public class OperadorConcatenacao {
+
+	public static void main(String[] args) {
+		String nomeCompleto = "LINGUAGEM " + "JAVA";
+		
+		System.out.println(nomeCompleto);
+		
+		//qual o resultado das expressoes abaixo?
+		String concatenacao ="?"; 
+
+		concatenacao = 1+1+1+"1";
+		System.out.println(concatenacao);
+		concatenacao = 1+"1"+1+1;
+		System.out.println(concatenacao);
+		concatenacao = 1+"1"+1+"1";
+		System.out.println(concatenacao);
+		concatenacao = "1"+1+1+1;
+		System.out.println(concatenacao);
+		concatenacao = "1"+(1+1+1);
+		System.out.println(concatenacao);
+	}
+
+}
---
+++ operadores/src/OperadoresDeAtribuicao.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+
+public class OperadoresDeAtribuicao {
+
+	public static void main(String[] args) {
+		String nome = "JAIR";
+		int idade = 22;
+		double peso = 68.5;
+		char sexo = 'M';
+		boolean doadorOrgao = false;
+		Date dataNascimento = new Date();
+		
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(peso);
+		System.out.println(sexo);
+		System.out.println(doadorOrgao);
+		System.out.println(dataNascimento);
+
+	}
+
+}
